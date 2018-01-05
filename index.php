a9e4a5fcd50d2fc810998e67da728af9e9855efe
nopchok <30373891+nopchok@users.noreply.github.com>
2018-01-05 17:32:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 
+$ch =  curl_init('https://bittrex.com/api/v1.1/public/getmarketsummaries');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 3);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json'));
+$bittrex = curl_exec($ch);
+$bit = json_decode($bittrex,true)['result'];
+
 $ch =  curl_init('https://poloniex.com/public?command=returnTicker');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
@@ -8,8 +18,12 @@ $ch =  curl_init('https://poloniex.com/public?command=returnTicker');
     curl_setopt($ch, CURLOPT_TIMEOUT, 3);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json'));
 $poloniex = curl_exec($ch);
+$polo = json_decode($poloniex,true);
 
-$ch =  curl_init('https://bittrex.com/api/v1.1/public/getmarketsummaries');
-$bittrex = curl_exec($ch);
+foreach( $polo as $k => $v ){
+    if( substr($k,0,3) === 'BTC' ){
+        $alt = str_replace("_","-",$k);
+    }
+}
 
 print_r($bittrex);
